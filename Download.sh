cf11773db9c7d095d98c8cb896751d893d719e21
Alex <alexliu0809@hotmail.com>
2015-11-06 12:10:41 +0000

    Revised Download.sh

--- Download.sh
+++ Download.sh
@@ -1,16 +1,32 @@
 #/bin/bash
 
-repository="git://github.com/alexliu0809/ScenarioWeek_Group4_UCL.git"
+repository="https://github.com/alexliu0809/ScenarioWeek_Group4_UCL.git"
 
 localFolder="./CODE/"
 
-#git init
+#echo "checking arguments"
+#if [ $* -le 0]
+#	exit
+#fi
+#echo "entering folder"
+#cd $1
+echo "creating folder for Data"
+mkdir CODE
+echo "entering folder"
+cd $localFolder
+echo "cloning the repository"
+git clone $repository
+#echo "initializing git folder"
+#git -C init
+#echo "connecting to remote server"
+#git remote add origin $repository
 
-echo "Fetching from github origin"
-git -C $localFolder fetch origin
-echo "change branch to master"
-git -C $localFolder checkout master
-echo "merge master and origin/master"
-git -C $localFolder merge origin/master
-echo "fetch and merge successfully"
+#echo "Fetching from github origin"
+#git -C $localFolder fetch origin
+#echo "change branch to master"
+#git -C $localFolder checkout master
+#echo "merge master and origin/master"
+#git -C $localFolder merge origin/master
+#echo "fetch and merge successfully"
+
 echo -e "\n"
